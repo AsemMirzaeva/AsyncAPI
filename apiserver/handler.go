91cdd4dc56f1525d736ff2a4906a3a7147c3424e
AsemMirzaeva <asemmirzaeva1@gmail.com>
2025-09-23 01:22:04 +0500

    Refresh token handler is added

--- apiserver/handler.go
+++ apiserver/handler.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -64,8 +66,6 @@ func (s *ApiServer) signupHandler() http.HandlerFunc {
 
 			return NewErrWithStatus(http.StatusConflict, fmt.Errorf("email already registered: %v", existingUser))
 		}
-
-		
 
 		_, err = s.store.Users.CreateUser(r.Context(), req.Email, req.Password)
 		if err != nil {
@@ -142,6 +142,81 @@ func (s *ApiServer) signInHandler() http.HandlerFunc {
 
 		if err := encode(ApiResponse[SignInResponse]{
 			Data: &SignInResponse{
+				AccessToken:  tokenPair.AccessToken.Raw,
+				RefreshToken: tokenPair.RefreshToken.Raw,
+			},
+		}, http.StatusOK, w); err != nil {
+			return NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+		return nil
+	})
+}
+
+type TokenRefreshReequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type TokenRefreshResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func (r TokenRefreshReequest) Validate() error {
+	if r.RefreshToken == "" {
+		return errors.New("refresh token is required")
+	}
+	return nil
+}
+
+func (s *ApiServer) tokenRefreshHandler() http.HandlerFunc {
+	return handler(func(w http.ResponseWriter, r *http.Request) error {
+		req, err := decode[TokenRefreshReequest](r)
+		if err != nil {
+			return NewErrWithStatus(http.StatusBadRequest, err)
+		}
+
+		currentRefreshToken, err := s.jwtManager.Parse(req.RefreshToken)
+		if err != nil {
+			return NewErrWithStatus(http.StatusUnauthorized, err)
+		}
+
+		userIdStr, err := currentRefreshToken.Claims.GetSubject()
+		if err != nil {
+			return NewErrWithStatus(http.StatusUnauthorized, err)
+		}
+
+		userId, err := uuid.Parse(userIdStr)
+		if err != nil {
+			return NewErrWithStatus(http.StatusUnauthorized, err)
+		}
+
+		currentRefreshTokenRecord, err := s.store.RefreshTokenStore.ByPrimaryKey(r.Context(), userId, currentRefreshToken)
+		if err != nil {
+			status := http.StatusInternalServerError
+			if errors.Is(err, sql.ErrNoRows) {
+				status = http.StatusUnauthorized
+			}
+			return NewErrWithStatus(status, err)
+		}
+
+		if currentRefreshTokenRecord.ExpiresAt.Before(time.Now()) {
+			return NewErrWithStatus(http.StatusUnauthorized, fmt.Errorf("refresh token expired"))
+		}
+
+		tokenPair, err := s.jwtManager.GenerateTokenPair(userId)
+		if err != nil {
+			return NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+
+		if _, err := s.store.RefreshTokenStore.DeleteUserTokens(r.Context(), userId); err != nil {
+			return NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+		if _, err := s.store.RefreshTokenStore.Create(r.Context(), userId, tokenPair.RefreshToken); err != nil {
+			return NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+
+		if err := encode(ApiResponse[TokenRefreshResponse]{
+			Data: &TokenRefreshResponse{
 				AccessToken:  tokenPair.AccessToken.Raw,
 				RefreshToken: tokenPair.RefreshToken.Raw,
 			},
