af88b79e7345cf4a85648413c0f990708b417410
AsemMirzaeva <asemmirzaeva1@gmail.com>
2025-10-01 21:13:19 +0500

    infra(terraform): add LocalStack configuration for local AWS resources

---
+++ cmd/awstest/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"asyncapi/config"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+func main() {
+	ctx := context.Background()
+	sdkConfig, err := awsconfig.LoadDefaultConfig(ctx)
+	if err != nil {
+		fmt.Println("Couldnt load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+
+	conf, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s3Client := s3.NewFromConfig(sdkConfig, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(conf.S3LocalstackEndpoint)
+		o.UsePathStyle = true
+	})
+	out, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, bucket := range out.Buckets {
+		fmt.Println(*bucket.Name)
+	}
+
+	sqsClient := sqs.NewFromConfig(sdkConfig, func(options *sqs.Options) {
+		options.BaseEndpoint = aws.String(conf.LocalstackEndpoint)
+	})
+
+	sqsOutput, err := sqsClient.ListQueues(ctx, &sqs.ListQueuesInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, q := range sqsOutput.QueueUrls {
+		fmt.Println(q)
+	}
+}
